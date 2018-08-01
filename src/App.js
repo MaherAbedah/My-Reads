43b7de2a6be8dcef7aef44575a74fcd554a87c4d
Maher-Abedah <m.abedah1@gmail.com>
2018-08-01 10:37:31 +0300

    feat: adding the search funcionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import BookSearch from './BookSearch'
 import BooksList from './BooksList'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    
+    books: [],
+    shelf: ''
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books : books
+      })
+      console.log(books)
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Route path='/search' render={()=>(
-          <BookSearch/>
+          <BookSearch onShelfupdate={()=>{}} />
         )}/>
         <Route exact path='/' render={()=>(
-          <BooksList/>
+          <BooksList onShelfupdate={()=>{}} books={this.state.books}/>
         )}/>
       </div>
     )
--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 
 class BookSearch extends Component {
+	state={
+		query: '',
+		showingBooks: []
+	}
+	updateQuery = (query) => {
+		this.setState({query : query.trim() })
+	}
 	render(){
+		
+		if(this.state.query){
+			BooksAPI.search(this.state.query).then((showingBooks)=>{
+				this.setState({
+					showingBooks: showingBooks
+				})
+			})
+		}
+
 		return (
 			<div className="search-books">
             <div className="search-books-bar">
@@ -13,16 +30,41 @@ class BookSearch extends Component {
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" 
+                	   placeholder="Search by title or author"
+                	   value={this.state.query}
+                	   onChange={(event)=>this.updateQuery(event.target.value)}/>
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+
+              	{ this.state.showingBooks.length > 0 &&
+              		this.state.showingBooks.map((book) =>
+                      <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks?  `url(${book.imageLinks.smallThumbnail})` : '' }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors ? book.authors.toString() : ''}</div>
+                        </div>
+                      </li>)}
+              	
+              </ol>
             </div>
           </div>
 			)
